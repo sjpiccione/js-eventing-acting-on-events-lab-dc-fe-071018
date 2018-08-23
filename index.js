f64451870c1a860059912ef80039a26bf86f89be
Steven Piccione <sjpiccione@gmail.com>
2018-08-23 22:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var dodger = document.getElementById('dodger');
   dodger.style.backgroundColor = 'salmon';
   
   dodger.style.bottom 
-    = '0px'
+    = '0px';
     
 document.addEventListener('keydown', function(e) {
-  console.log(e.which)
-})
+  console.log(e.which);
+});
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
  
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left - 1}px`;
   }
-})
+});
