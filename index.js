11420d3890efe4e054ed1def8b6310efd1e86e31
Steven Piccione <sjpiccione@gmail.com>
2018-08-23 22:53:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+});
