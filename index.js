cbd45551aca3171b130e537185345c6e09b423c6
Steven Piccione <sjpiccione@gmail.com>
2018-08-23 22:51:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
 
-
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
