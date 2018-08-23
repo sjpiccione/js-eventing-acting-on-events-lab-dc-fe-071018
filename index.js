54f1792b6650a3515b66e0becd3d281b12aa575c
Steven Piccione <sjpiccione@gmail.com>
2018-08-23 22:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '');
   var right = parseInt(rightNumbers, 10);
  
-  if (right > 0) {
+  if (left > 0) {
     dodger.style.right = `${left + 1}px`;
   }
 }
